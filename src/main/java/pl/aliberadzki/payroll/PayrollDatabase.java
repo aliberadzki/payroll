b4f494d36e64df5a5b76c7f0e82bc86bc3f32a61
Adam Liberadzki <adam.liberadzki@gmail.com>
2016-07-01 21:56:08 +0200

    Implemented AddSalariedEmployeetransaction. DB is mocked badly

---
+++ src/main/java/pl/aliberadzki/payroll/PayrollDatabase.java
@@ -0,0 +1,7 @@
+package pl.aliberadzki.payroll;
+
+/**
+ * Created by aliberadzki on 01.07.16.
+ */
+public class PayrollDatabase {
+}
---
+++ src/test/java/pl/aliberadzki/payroll/AddEmployeeTransactionTest.java
@@ -0,0 +1,26 @@
+package pl.aliberadzki.payroll;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by aliberadzki on 01.07.16.
+ */
+public class AddEmployeeTransactionTest {
+
+    @Test
+    public void testGetClassification() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSchedule() throws Exception {
+
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+
+    }
+}
