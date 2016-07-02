c300bc00788f56912878ac680227b699d62cd15b
Adam Liberadzki <adam.liberadzki@gmail.com>
2016-07-02 06:22:57 +0200

    added methods to AddCommisionedEmployee

--- src/main/java/pl/aliberadzki/payroll/AddCommisionedEmployee.java
+++ src/main/java/pl/aliberadzki/payroll/AddCommisionedEmployee.java
@@ -3,5 +3,14 @@ package pl.aliberadzki.payroll;
 /**
  * Created by aliberadzki on 01.07.16.
  */
-public class AddCommisionedEmployee {
+public class AddCommisionedEmployee extends AddEmployeeTransaction {
+    @Override
+    public PaymentClassification getClassification() {
+        return null;
+    }
+
+    @Override
+    public PaymentSchedule getSchedule() {
+        return new BiweeklySchedule();
+    }
 }
